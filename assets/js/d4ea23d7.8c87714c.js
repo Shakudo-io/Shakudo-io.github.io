c401fa24fe453a742fb157a06803e47dc3733d77
Aki Kim <aki@shakudo.io>
2022-07-21 10:05:03 -0400

    update to docs

--- assets/js/d4ea23d7.8c87714c.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self.webpackChunkmy_website=self.webpackChunkmy_website||[]).push([[7128],{3393:function(e){e.exports=JSON.parse('{"label":"intro","permalink":"/tags/intro","allTagsPath":"/tags","count":1,"items":[{"id":"intro","title":"Welcome to Shakudo","description":"Get started","permalink":"/"}]}')}}]);
