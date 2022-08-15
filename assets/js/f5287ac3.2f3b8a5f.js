ab83b5984957a1295dab675e9f859f840d3b7691
Aki Kim <aki@shakudo.io>
2022-08-15 14:21:38 -0400

    building doc v1.4

--- assets/js/f5287ac3.2f3b8a5f.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self.webpackChunkmy_website=self.webpackChunkmy_website||[]).push([[2855],{3791:function(e){e.exports=JSON.parse('{"label":"get started","permalink":"/tags/get-started","allTagsPath":"/tags","count":1,"items":[{"id":"intro","title":"Welcome to Shakudo","description":"Get started","permalink":"/home"}]}')}}]);
